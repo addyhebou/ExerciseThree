f401476f60122b9b573d66f326e771b8441234a8
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-03-25 16:20:20 -0400

    final to send to teacher

--- exercise-three/routes/index.js
+++ exercise-three/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("I ain't f a b in so long I'll do it in a Honda Accord"))
+router.get("/", (req, res) => res.send("I'll do it in a Honda Accord"))
 
 module.exports = router;
