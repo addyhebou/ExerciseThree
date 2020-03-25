6c1c7f28d731e7571df8042a67d1f982b12461fa
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-03-25 09:51:36 -0400

    Wednesday, March 25

--- exercise-three/app.js
+++ exercise-three/app.js
@@ -6,5 +6,12 @@ const port = 4000;
 app.get('/', (req, res) => (res.send('<h1>Wassup yall</h1><p>Hi, Im Paul, ya know?</p>')))
 // .get indicates that it is a child of app
 // function being called
+const indexRoute = require('./routes/index.js')
+const aboutRoute = require('./routes/about.js')
+
+
+app.use("/", indexRoute);
+app.use("/about", aboutRoute);
+app.use("/static", express.static('public'))
 app.listen(port, () => console.log('Your program is now running...'))
 // function being called
---
+++ exercise-three/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (req, res) => res.send("I ain't f a b in so long I'll do it in a Honda Accord"))
+
+module.exports = router;
